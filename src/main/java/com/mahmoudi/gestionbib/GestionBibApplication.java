4785e005e45a50fbe051bd3172f60a6c11107559
MAHMOUDI-FAROUK <118342402+MAHMOUDI-FAROUK@users.noreply.github.com>
2024-01-30 15:27:55 +0100

    Add files via upload

---
+++ src/main/java/com/mahmoudi/gestionbib/GestionBibApplication.java
@@ -0,0 +1,13 @@
+package com.mahmoudi.gestionbib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBibApplication.class, args);
+	}
+
+}
